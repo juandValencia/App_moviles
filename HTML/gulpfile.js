eb4a832e356bcc8dc5f8dcf58ff80dcc53f40b26
juandValencia <juand.valenciao@upb.edu.co>
2021-03-05 08:44:53 -0500

    sass y gulp
    Sass y gulp instalados ,para el desarrollo fluido del equipo

---
+++ HTML/gulpfile.js
@@ -0,0 +1,32 @@
+const {src,dest}=require("gulp");
+const sass =require("gulp-sass");
+const rename=require("gulp-rename");
+const cleanCSS=rename("gulp-clean-css");
+const concat=require("gulp-concat");
+const uglify=require("gulp-uglify-es").default;
+
+sass.compiler=require("dart-sass");
+function css() {
+  // place code for your default task here
+  return src("./sass/**/*.scss")
+   			.pipe(sass())
+  			.pipe(cleanCSS())
+  			.pipe(rename("main.init.css"))
+  			.pipe(dest("./css"));
+}
+function js(){
+	return src("./js¨/**/*.js");
+		.pipe(concat("main.mim.js"))
+		.pipe(dest("./css"))
+}
+function sass(){
+	 return src("./sass/**/*.scss")
+   			.pipe(sass())
+   			.pipe(rename("main.init.css"))
+  			.pipe(dest("./css"));
+
+}
+
+exports.js=js;
+exports.css=css;
+exports.default=()=>{};
