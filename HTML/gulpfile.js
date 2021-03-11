b0576bc91905d8d9cfcdfd35cc79470c43fc64ea
juandValencia <juand.valenciao@upb.edu.co>
2021-03-11 17:23:06 -0500

    carpeta budle completa
    Se configuro sass gulp  completamente

--- HTML/gulpfile.js
+++ HTML/gulpfile.js
@@ -1,7 +1,7 @@
 const {src,dest}=require("gulp");
 const sass =require("gulp-sass");
 const rename=require("gulp-rename");
-const cleanCSS=rename("gulp-clean-css");
+const cleanCSS=require("gulp-clean-css");
 const concat=require("gulp-concat");
 const uglify=require("gulp-uglify-es").default;
 
@@ -11,22 +11,22 @@ function css() {
   return src("./sass/**/*.scss")
    			.pipe(sass())
   			.pipe(cleanCSS())
-  			.pipe(rename("main.init.css"))
+  			.pipe(rename("main.mim.css"))
   			.pipe(dest("./bundle/css"));
 }
 function js(){
-	return src("./js¨/**/*.js");
-		.pipe(concat("main.mim.js"))
-		.pipe(dest("./bundle/js"))
+  return src("./js/**/*.js")
+    .pipe(concat("main.mim.js"))
+    .pipe(uglify())
+    .pipe(dest("./bundle/js"));
 }
-function sass(){
-	 return src("./sass/**/*.scss")
-   			.pipe(sass())
-   			.pipe(rename("main.init.css"))
-  			.pipe(dest("./bundle"));
 
-}
+
+
 
 exports.js=js;
+
+
+
 exports.css=css;
 exports.default=()=>{};
